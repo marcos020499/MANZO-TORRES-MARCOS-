599344916b722670292aab2f8e91f5a2c9e60c48
marcos020499 <46718805+marcos020499@users.noreply.github.com>
2020-03-22 09:53:14 -0600

    Add files via upload

---
+++ MANZO_TORRES_MARCOS/PROGRAMACIÓN_DE_SISTEMAS_EMBEBIDOS/PRÁCTICAS/GCC_ARM/gcc.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num, dig1, dig2, dig3, dig4, dig5, dig6, dig7, dig8, dig9, dig10;
+    cout<<"ingrese un numero, lo sumaremos de 2 en 2  hasta repetirlo 10 veces"<<endl;
+    cin>>num;
+    dig1=num+2;
+    dig2=num+4;
+    dig3=num+6;
+    dig4=num+8;
+    dig5=num+10;
+    dig6=num+12;
+    dig7=num+14;
+    dig8=num+16;
+    dig9=num+18;
+    dig10=num+20;
+    cout<<"el primer numero es  "<<  dig1<<endl;
+    cout<<"el segundo numero es  "<<dig2<<endl;
+    cout<<"el tercer numero es  "<<dig3<<endl;
+    cout<<"el cuarto numero es  "<<dig4<<endl;
+    cout<<"el quinto numero es  "<<dig5<<endl;
+    cout<<"el sexto numero es  "<<dig6<<endl;
+    cout<<"el septimo numero es  "<<dig7<<endl;
+    cout<<"el octavo numero es  "<<dig8<<endl;
+    cout<<"el noveno numero es  "<<dig9<<endl;
+    cout<<"el decimo numero es  "<<dig10<<endl;
+return 0;
+}
